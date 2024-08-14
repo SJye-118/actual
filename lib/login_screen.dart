544029f63571fc1583cbeafd19b2ae8d6850a9e4
SJye-118 <shijye118@gamil.com>
2024-08-14 12:13:00 +0800

    CHAD

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -30,46 +30,51 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Log In'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Add your logo image here
-            Image.asset(
-              'assets/Gigachad.jpg', // Path to your logo image
-              height: 150, // Adjust the height as needed
-            ),
-            const SizedBox(
-                height: 40), // Add some space between the image and text fields
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: 'Name'),
-            ),
-            TextField(
-              controller: _passwordController,
-              obscureText: true, // Hide password input
-              decoration: InputDecoration(labelText: 'Password'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _login(context),
-              child: Text('Log In'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        SignUpScreen(), // Navigate to sign-up page
-                  ),
-                );
-              },
-              child: Text('Sign Up'),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Add your logo image here
+              Image.asset(
+                'assets/Gigachad.jpg', // Path to your logo image
+                height: 150, // Adjust the height as needed
+              ),
+              const SizedBox(height: 40), // Space between image and text fields
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Name'),
+              ),
+              TextField(
+                controller: _passwordController,
+                obscureText: true, // Hide password input
+                decoration: InputDecoration(labelText: 'Password'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _login(context),
+                child: Text('Log In'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          SignUpScreen(), // Navigate to sign-up page
+                    ),
+                  );
+                },
+                child: Text('Sign Up'),
+              ),
+            ],
+          ),
         ),
       ),
     );
